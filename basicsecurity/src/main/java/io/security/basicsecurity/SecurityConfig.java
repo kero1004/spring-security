4e503e3a9f77da8a6ffe34fbc6aff4a83cfe2b48
kero <jdy410@naver.com>
2021-10-19 16:42:27 +0900

    Logout 처리

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -8,11 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @EnableWebSecurity  // 웹 보안 활성화 시키기 위함
@@ -27,29 +29,50 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         //인증 정책 - formLogin 설정
-        http.formLogin()
-        .loginPage("/loginPage")                              // 사용자 정의 로그인 페이지
-        .defaultSuccessUrl("/")                                 // 인증 성공시 이동할 수 있는 url
-                        .failureUrl("/login")                   // 인증 실패시 이동하는 url
-                        .usernameParameter("userId")            // username의 name명 - default는 username
-                        .passwordParameter("passwd")            // password의 name명 - default는 password
-                        .loginProcessingUrl("/login_proc")      // form태그의 action url설정 - default는 /login
-                        .successHandler(new AuthenticationSuccessHandler() {
-                            @Override
-                            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-                                // authentication : 인증에 성공했을때 인증한 결과를 담은 인증 객체
+        http
+                .formLogin()
+                //.loginPage("/loginPage")              // 사용자 정의 로그인 페이지
+                .defaultSuccessUrl("/")                 // 인증 성공시 이동할 수 있는 url
+                .failureUrl("/login")                   // 인증 실패시 이동하는 url
+                .usernameParameter("userId")            // username의 name명 - default는 username
+                .passwordParameter("passwd")            // password의 name명 - default는 password
+                .loginProcessingUrl("/login_proc")      // form태그의 action url설정 - default는 /login
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        // authentication : 인증에 성공했을때 인증한 결과를 담은 인증 객체
 
-                                System.out.println("authentication " + authentication.getName());
-                httpServletResponse.sendRedirect("/");  // 루트 페이지로 이동
-            }
-        })   //로그인 성공 핸들러 - 우리는 익명클래스 사용
-        .failureHandler(new AuthenticationFailureHandler() {
-            @Override
-            public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                System.out.println("exception " + e.getMessage());
-                httpServletResponse.sendRedirect("/login");
-            }
-        })  // 실패 핸들러
-        .permitAll();   // 누구나 접근 가능하도록 해야한다. ( permitAll 을 하지않으면 젤 위에 인가정책때문에 /loginPage 에 접근이 불가능하다.
+                        System.out.println("authentication " + authentication.getName());
+                    httpServletResponse.sendRedirect("/");  // 루트 페이지로 이동
+                    }
+                })   //로그인 성공 핸들러 - 우리는 익명클래스 사용
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        System.out.println("exception " + e.getMessage());
+                        httpServletResponse.sendRedirect("/login");
+                    }
+                })  // 실패 핸들러
+                .permitAll();   // 누구나 접근 가능하도록 해야한다. ( permitAll 을 하지않으면 젤 위에 인가정책때문에 /loginPage 에 접근이 불가능하다.
+
+        //로그아웃
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me");
     }
 }
